21768de7216b0ccd57ab67aee1e7b5016a2f5c6c
Seth-Joseph <61108580+Seth-Joseph@users.noreply.github.com>
2021-06-22 09:53:26 +0530

    Update RemoveFiles.py

--- RemoveFiles.py
+++ RemoveFiles.py
@@ -1,11 +1,11 @@
-import os;
-import shutil;
-import time;
+import os, time
 
-path = input("Enter path of the file to delete :")
+path = r"Enter file path here"                #<--- enter file path here
 now = time.time()
-f = os.path.join(path,f)
-for f in os.listdir(path) :
-    if os.stat(f).st_mtime < now - 7 *86400 :
-        if os.path.isfile(f):
-            os.remove(os.path.join(path,f))
+
+for filename in os.listdir(path):
+    if os.path.getmtime(os.path.join(path, filename)) < now - 30 * 86400:
+        if os.path.isfile(os.path.join(path, filename)):
+            print(filename)
+            os.remove(os.path.join(path, filename))
+            print("File Deleted:",filename)
